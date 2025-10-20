cf291ce86a08c7873b98b0981e03f976eb405300
Allan Too <allankim540@gmail.com>
2025-10-20 13:05:53 +0300

    Final commit

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    const handleChange =(e)=>{
+        console.log("Entering password...");
+    }
     return (
-        <div></div>
-    )
+      <div>
+        <input type="password" onChange={handleChange} />
+      </div>
+    );
 }
 
 export default Keypad;
